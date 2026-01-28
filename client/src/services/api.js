3d8ae9ec44e16fb5b974600154ee58769787b682
shivanjali saini <shivanjali@techprocompsoft.com>
2026-01-28 11:19:15 +0530

    server url updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://inventory-system-5z75.onrender.com/api',
 });
 
 export const getProducts = () => api.get('/products');
